05ac598190285cef98ab898c9f19eacda673e832
Aashish-kumar01 <kumarashish69959@gmail.com>
2026-09-12 01:45:05 +0530

    Updated File

--- SprngScrtyFirst/src/main/java/com/SpringSec/SprngScrtyFirst/config/SecurityConfig.java
+++ SprngScrtyFirst/src/main/java/com/SpringSec/SprngScrtyFirst/config/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsPasswordService;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -36,7 +37,8 @@ public class SecurityConfig {
     @Bean
     public AuthenticationProvider authenticationProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+//        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
         return provider;
     }
 
--- SprngScrtyFirst/src/main/java/com/SpringSec/SprngScrtyFirst/service/MyUserDetailsService.java
+++ SprngScrtyFirst/src/main/java/com/SpringSec/SprngScrtyFirst/service/MyUserDetailsService.java
@@ -1,5 +1,6 @@
 package com.SpringSec.SprngScrtyFirst.service;
 
+import com.SpringSec.SprngScrtyFirst.model.UserPrincipal;
 import com.SpringSec.SprngScrtyFirst.model.Users;
 import com.SpringSec.SprngScrtyFirst.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +16,10 @@ public class MyUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Users user = repo.findByUsername(username);
-        return null;
+        if(user == null){
+            System.out.println("User not Found");
+            throw new UsernameNotFoundException("user not found");
+        }
+        return new UserPrincipal(user);
     }
 }
